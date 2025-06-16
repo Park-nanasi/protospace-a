6c5034f95c6162a700062e71631c7e2c0e0b1563
Park-nanasi <park.sungjin@smart-group.co.jp>
2025-06-16 15:35:34 +0900

    first commit

---
+++ src/main/java/in/tech_camp/protospace_a/ProtospaceAApplication.java
@@ -0,0 +1,13 @@
+package in.tech_camp.protospace_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtospaceAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtospaceAApplication.class, args);
+	}
+
+}
