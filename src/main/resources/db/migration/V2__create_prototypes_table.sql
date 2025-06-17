856c6483d4ac596bd61241814efaf18125f9756a
hayato.sawamura <hayato.sawamura@smart-gruop.co.jp>
2025-06-17 11:22:09 +0900

    :zap: ＤＢのマイグレーションファイルの作成
    users, prototyps, commentsのテーブルを作成

---
+++ src/main/resources/db/migration/V2__create_prototypes_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE prototypes (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    catchphrase VARCHAR(128) NOT NULL,
+    concept VARCHAR(512) NOT NULL,
+    image VARCHAR(256) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
