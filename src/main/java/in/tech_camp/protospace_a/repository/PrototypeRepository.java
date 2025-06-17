ea9f095fc071a48c3064de30c378abe51bfaecb9
hayato.sawamura <hayato.sawamura@smart-gruop.co.jp>
2025-06-17 18:02:51 +0900

    :zap: プロトタイプ削除機能の挙動確認

--- src/main/java/in/tech_camp/protospace_a/repository/PrototypeRepository.java
+++ src/main/java/in/tech_camp/protospace_a/repository/PrototypeRepository.java
@@ -2,6 +2,7 @@ package in.tech_camp.protospace_a.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
