91a79dbd54ae2d4d83e0933f185f23a898e4cea6
hayato.sawamura <hayato.sawamura@smart-gruop.co.jp>
2025-06-17 15:24:01 +0900

    :zap: プロトタイプをDBに保存する機能作成

--- src/main/java/in/tech_camp/protospace_a/repository/PrototypeRepository.java
+++ src/main/java/in/tech_camp/protospace_a/repository/PrototypeRepository.java
@@ -2,6 +2,7 @@ package in.tech_camp.protospace_a.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -9,6 +10,9 @@ import in.tech_camp.protospace_a.entity.PrototypeEntity;
 
 @Mapper
 public interface PrototypeRepository {
+  @Insert("INSERT INTO prototypes (name, catchphrase, concept, image) VALUES (#{name}, #{catchphrase}, #{concept}, #{image}) ")
+  void insertPrototype(PrototypeEntity prototype);
+
   // todo: user追加（user機能作成後）
   @Select("SELECT name, catchphrase, image FROM prototypes")
   List<PrototypeEntity> getAllPrototypes();
