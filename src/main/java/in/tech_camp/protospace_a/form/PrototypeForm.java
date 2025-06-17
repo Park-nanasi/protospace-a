4ba79c0e39de07878d4c17710ca24d1c7c5d1115
hayato.sawamura <hayato.sawamura@smart-gruop.co.jp>
2025-06-17 15:04:24 +0900

    :seedling: ユーザーからプロトタイプの投稿情報を受け取るフォームクラス作成

---
+++ src/main/java/in/tech_camp/protospace_a/form/PrototypeForm.java
@@ -0,0 +1,11 @@
+package in.tech_camp.protospace_a.form;
+
+import lombok.Data;
+
+@Data
+public class PrototypeForm {
+  private String name;
+  private String catchphrase;
+  private String concept;
+  private String image;
+}
