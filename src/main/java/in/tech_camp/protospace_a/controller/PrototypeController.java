ad0e68e564603f85f71513c12e762011e949f4ec
Park-nanasi <park.sungjin@smart-group.co.jp>
2025-06-18 17:29:12 +0900

    プロトタイプ投稿ページ作成

--- src/main/java/in/tech_camp/protospace_a/controller/PrototypeController.java
+++ src/main/java/in/tech_camp/protospace_a/controller/PrototypeController.java
@@ -17,6 +17,8 @@ import in.tech_camp.protospace_a.form.PrototypeForm;
 import in.tech_camp.protospace_a.repository.PrototypeRepository;
 import lombok.AllArgsConstructor;
 
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -29,17 +31,53 @@ public class PrototypeController {
   public String getMethodName() {
       return "tmp/test";
   }
+  
 
   // testディレクトリ内は挙動確認を行っています。
-  @GetMapping("/test/prototype")
-  public String showAllPrototypes(Model model) {
+  @GetMapping("/proto/new")
+  public String showProtoDetail(Model model) {
     List<PrototypeEntity> prototypes =  prototypeRepository.getAllPrototypes();
     System.out.println("prototypes:" + prototypes);
 
     // createPrototypeの挙動を確認するため
     PrototypeForm newPrototypeForm = new PrototypeForm();
     model.addAttribute("prototypeForm", newPrototypeForm);
-    return "tmp/prototype";
+    return "proto/new";
+  }
+
+  @PostMapping("/proto")
+  public String createProto(@ModelAttribute("prototypeForm") @Validated PrototypeForm prototypeForm, BindingResult bindingResult, Model model) {
+    // todo ログイン機能作成後
+    // - ログイン状態の場合のみ、投稿ページへ遷移できること。
+    // - ログアウト状態で投稿ページに遷移しようとすると、ログインページに遷移すること
+    if (bindingResult.hasErrors()) {
+        model.addAttribute("errors", bindingResult.getAllErrors()
+            .stream()
+            .map(error -> error.getDefaultMessage())
+            .collect(Collectors.toList()));
+        return "proto/new";
+    }
+
+    // todo プロトタイプの投稿画面作成後
+    // - 投稿に必要な情報が入力されていない場合は、投稿できずにそのページに留まること。
+    // - バリデーションによって投稿ができず、そのページに留まった場合でも、入力済みの項目（画像以外）は消えないこと
+
+    PrototypeEntity prototype = new PrototypeEntity();
+    prototype.setName(prototype.getName());
+    prototype.setConcept(prototype.getConcept());
+    prototype.setCatchphrase(prototype.getCatchphrase());
+    prototype.setImages(prototype.getImages());
+
+    try {
+      prototypeRepository.insertPrototype(prototype);
+    } catch (Exception e) {
+      System.err.println("Error：" + e);
+      return "redirect:/";
+    }
+
+    // todo トップページ作成後
+    // - 正しく投稿できた場合は、トップページへ遷移すること。
+    return "redirect:/";
   }
 
   @PostMapping("/test/prototype")
