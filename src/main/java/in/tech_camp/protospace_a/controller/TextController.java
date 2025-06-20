87353135c706c3cb035c19ad208d63949b15969b
Park-nanasi <park.sungjin@smart-group.co.jp>
2025-06-20 10:14:07 +0900

    詳細ページの生成・画像表示バグ修正

--- src/main/java/in/tech_camp/protospace_a/controller/TextController.java
+++ src/main/java/in/tech_camp/protospace_a/controller/TextController.java
@@ -1,24 +1,38 @@
 package in.tech_camp.protospace_a.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import in.tech_camp.protospace_a.entity.PrototypeEntity;
+import in.tech_camp.protospace_a.repository.PrototypeRepository;
 import lombok.AllArgsConstructor;
 
 @Controller
 @AllArgsConstructor
 public class TextController {
 
+  private final PrototypeRepository prototypeRepository;
+
   @GetMapping("/")
-  public String showTopPage(Model model) {
-      // 必要があれば model に属性を追加できます
-      return "proto/index"; // templates/proto/index.html を表示
+    public String showAllPrototypes(Model model) {
+    List<PrototypeEntity> prototypes =  prototypeRepository.findAllPrototypes();
+    model.addAttribute("prototypes", prototypes);
+    System.out.println("prototypes: " + prototypes);
+    return "prototypes/index";
   }
 
   @GetMapping("/detail")
   public String showProtoDetail(Model model) {
       // 必要があれば model に属性を追加できます
-      return "proto/detail";
+      return "prototypes/detail";
+  }
+
+  @GetMapping("/users/1")
+  public String showUserPage(Model model) {
+      // 必要があれば model に属性を追加できます
+      return "users/userInfo";
   }
 }
