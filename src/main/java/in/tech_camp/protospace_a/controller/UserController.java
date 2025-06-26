05220cd942406855997e4ca52ad5f5ffb9b365c1
hayato.sawamura <hayato.sawamura@smart-gruop.co.jp>
2025-06-26 10:44:19 +0900

    :zap: 新規登録のエラーメッセージの表示形式を変更

--- src/main/java/in/tech_camp/protospace_a/controller/UserController.java
+++ src/main/java/in/tech_camp/protospace_a/controller/UserController.java
@@ -1,12 +1,13 @@
 package in.tech_camp.protospace_a.controller;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -43,11 +44,9 @@ public class UserController {
     userForm.validateUserForm(result);
 
     if (result.hasErrors()) {
-      List<String> errorMessages = result.getAllErrors().stream()
-              .map(DefaultMessageSourceResolvable::getDefaultMessage)
-              .collect(Collectors.toList());
-
-      model.addAttribute("errorMessages", errorMessages);
+      Map<String, String> fieldErrors = result.getFieldErrors().stream()
+              .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage, (msg1, msg2) -> msg1));
+      model.addAttribute("fieldErrors", fieldErrors);
       model.addAttribute("userForm", userForm);
       return "users/signUp";
     }
