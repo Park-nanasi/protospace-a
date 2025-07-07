404ae82edf572b8300f8dc9dc852cd7e4c39fb3e
Park-nanasi <park.sungjin@smart-group.co.jp>
2025-07-07 14:19:50 +0900

    コメント一覧のデザイン変更

--- src/main/java/in/tech_camp/protospace_a/repository/CommentRepository.java
+++ src/main/java/in/tech_camp/protospace_a/repository/CommentRepository.java
@@ -16,14 +16,20 @@ import in.tech_camp.protospace_a.entity.CommentEntity;
 
 @Mapper
 public interface CommentRepository {
-  @Select("SELECT c.*, u.id AS user_id, u.username AS user_username FROM comments c JOIN users u ON c.user_id = u.id WHERE c.prototype_id = #{prototypeId}")
-  @Results(value = {@Result(property = "user.id", column = "user_id"),
-      @Result(property = "user.username", column = "user_username"),
-      @Result(property = "created_at", column = "created_at"),
-      @Result(property = "updated_at", column = "updated_at"),
-      @Result(property = "prototype", column = "prototype_id", one = @One(
-          select = "in.tech_camp.protospace_a.repository.PrototypeRepository.findById"))})
-  List<CommentEntity> findByPrototypeId(Integer prototypeId);
+  @Select("SELECT c.*, u.id AS user_id, u.username AS user_username, u.profile_image AS profile_image " +
+        "FROM comments c " +
+        "JOIN users u ON c.user_id = u.id " +
+        "WHERE c.prototype_id = #{prototypeId}")
+    @Results(value = {
+        @Result(property = "user.id", column = "user_id"),
+        @Result(property = "user.username", column = "user_username"),
+        @Result(property = "user.profileImage", column = "profile_image"),
+        @Result(property = "created_at", column = "created_at"),
+        @Result(property = "updated_at", column = "updated_at"),
+        @Result(property = "prototype", column = "prototype_id",
+                one = @One(select = "in.tech_camp.protospace_a.repository.PrototypeRepository.findById"))
+    })
+    List<CommentEntity> findByPrototypeId(Integer prototypeId);
 
   @Select("SELECT c.*, u.id AS user_id, u.username AS user_username, u.profile_image AS profile_image "
       + "FROM comments c " + "JOIN users u ON c.user_id = u.id "
